0f3ce0213e16925ed575091a8041d635409885f9
Phuc <hoangphuc2984@gmail.com>
2023-08-27 13:57:44 +0700

    [FIX] web_group_expand: fix click button Expand and Compress goes to the first page

--- web_group_expand/static/src/js/list_controller.esm.js
+++ web_group_expand/static/src/js/list_controller.esm.js
@@ -5,53 +5,18 @@ import {ListController} from "@web/views/list/list_controller";
 
 patch(ListController.prototype, "web_group_expand.ListController", {
     async expandAllGroups() {
-        // We expand layer by layer. So first we need to find the highest
-        // layer that's not already fully expanded.
-        let layer = this.model.root.groups;
-        while (layer.length) {
-            const closed = layer.filter(function (group) {
-                return group.isFolded;
-            });
-            if (closed.length) {
-                // This layer is not completely expanded, expand it
-                await layer.forEach((group) => {
-                    group.isFolded = false;
-                });
-                break;
+        $("tbody.ui-sortable > tr.o_group_header").each(function (idx, el) {
+            if (!$(el).attr("class").includes("o_group_open")) {
+                $(el).click();
             }
-            // This layer is completely expanded, move to the next
-            layer = _.flatten(
-                layer.map(function (group) {
-                    return group.list.groups || [];
-                }),
-                true
-            );
-        }
-        await this.model.load();
+        });
     },
 
     async collapseAllGroups() {
-        // We collapse layer by layer. So first we need to find the deepest
-        // layer that's not already fully collapsed.
-        let layer = this.model.root.groups;
-        while (layer.length) {
-            const next = _.flatten(
-                layer.map(function (group) {
-                    return group.list.groups || [];
-                }),
-                true
-            ).filter(function (group) {
-                return !group.isFolded;
-            });
-            if (!next.length) {
-                // Next layer is fully collapsed, so collapse this one
-                await layer.forEach((group) => {
-                    group.isFolded = true;
-                });
-                break;
+        $("tbody.ui-sortable > tr.o_group_header").each(function (idx, el) {
+            if ($(el).attr("class").includes("o_group_open")) {
+                $(el).click();
             }
-            layer = next;
-        }
-        await this.model.load();
+        });
     },
 });
